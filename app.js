1b3dd6e14364ed0e4733a5874e1b9e158ce91f91
udith <udithbiju2@gmail.com>
2024-07-29 23:29:40 +0530

    added counter

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 import express from "express";
-import "body-parser";
 import findNearestRanks from "./nearestRank.js";
+import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 app.set("view engine", "ejs");
+mongoose.connect(
+	"mongodb+srv://admin:admin@cluster0.eqevsdm.mongodb.net/keam?retryWrites=true&w=majority&appName=Cluster0",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
+
+const visitorSchema = new mongoose.Schema({
+	count: {
+		type: Number,
+		default: 0,
+	},
+});
+
+const Count = mongoose.model("Visitor", visitorSchema);
 
  let out = ''	; 
  
 // Route to render index.ejs 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
 	res.render("index", { colleges: [],rank:'',caste:'',course:'',out:''}); // Initial render without colleges
+	let count = await Count.findOne();
+	if (!count) {
+		count = new Count();
+	}
+	count.count+=1;
+	await count.save();	
 });
 
 app.post("/colleges", (req, res) => {
@@ -23,12 +45,11 @@ app.post("/colleges", (req, res) => {
   colleges.length>0?out='':out='No colleges found';
 
 	res.render("index", { colleges ,caste,rank,course,out}); // Render index.ejs with colleges data
+
 });
 
 // Start server
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
   
